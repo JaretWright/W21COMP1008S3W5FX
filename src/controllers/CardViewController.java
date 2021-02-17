41cadcbe7b2befc8f0621c06e50eed0d4bf4178c
JaretWright <jaret.wright@georgiancollege.ca>
2021-02-17 17:57:01 -0500

    Created a visual "hand of cards"

--- src/controllers/CardViewController.java
+++ src/controllers/CardViewController.java
@@ -1,11 +1,18 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import models.*;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -41,5 +48,18 @@ public class CardViewController implements Initializable {
         suitLabel.setText(card.getSuit());
         faceValueLabel.setText(Integer.toString(card.getFaceValue()));
         imageView.setImage(card.getCardImage());
+    }
+
+    @FXML
+    private void changeToHandOfCards(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../views/handOfCardsView.fxml"));
+        Scene scene = new Scene(root);
+
+        //This line gets the Stage information
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.setTitle("Hand of cards");
+        stage.show();
     }
 }
