c6316d581a77e12a43160e8a1060ce82087ca6c0
JaretWright <jaret.wright@georgiancollege.ca>
2021-02-17 16:35:52 -0500

    Updated so that a Card(or at least card labels) would be displayed in the view

--- src/controllers/CardViewController.java
+++ src/controllers/CardViewController.java
@@ -1,4 +1,32 @@
 package controllers;
 
-public class CardViewController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import models.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CardViewController implements Initializable {
+
+    @FXML
+    private Label faceNameLabel;
+
+    @FXML
+    private Label suitLabel;
+
+    @FXML
+    private Label faceValueLabel;
+
+    private DeckOfCards deckOfCards;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        deckOfCards = new DeckOfCards();
+        Card card = deckOfCards.dealTopCard();
+        faceNameLabel.setText(card.getFaceName());
+        suitLabel.setText(card.getSuit());
+        faceValueLabel.setText(Integer.toString(card.getFaceValue()));
+    }
 }
